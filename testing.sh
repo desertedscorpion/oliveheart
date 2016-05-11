35b8f013d5d53d9e849763481bef99aa1f6bb8c7
Emory Merryman <emory.merryman@gmail.com>
2016-05-11 21:26:49 +0000

    do not cleanup

--- testing.sh
+++ testing.sh
@@ -12,7 +12,7 @@ docker build -t ninthgrimmercury/oliveheart . &&
 	    true
     fi &&
     docker run --interactive --tty --privileged --detach --volume /sys/fs/cgroup:/sys/fs/cgroup:ro freakygamma/oliveheart &&
-    docker rm $(docker stop $(docker ps -a -q --filter ancestor=freakygamma/olivehear --format="{{.ID}}")) &&
-    docker rmi --force freakygamma/oliveheart &&
-    docker rmi --force ninthgrimmercury/oliveheart &&
+#    docker rm $(docker stop $(docker ps -a -q --filter ancestor=freakygamma/olivehear --format="{{.ID}}")) &&
+#    docker rmi --force freakygamma/oliveheart &&
+#    docker rmi --force ninthgrimmercury/oliveheart &&
     true
