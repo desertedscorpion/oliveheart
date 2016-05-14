15bc5bf79bd336ae23f4af5934059db10e25e272
Emory Merryman <emory.merryman@gmail.com>
2016-05-14 13:59:47 +0000

    fixed sed command and changed password temporarily to password

--- oliveheart.sh
+++ oliveheart.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 PASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1) &&
+    PASSWORD=password && // remove me
     echo -en "\n\n${PASSWORD}\n${PASSWORD}\n\n\n\n" | mysql_secure_installation &&
-    sed -e "s#password#${PASSWORD}" /opt/oliveheart/init.sql | mysql -u root --password=${PASSWORD} &&
+    sed -e "s#password#${PASSWORD}#" /opt/oliveheart/init.sql | mysql -u root --password=${PASSWORD} &&
     sed -e "s#username: root#username: redmine#" -e "s#password: \"\"#password: \"${PASSWORD}\"#" -e "w/opt/oliveheart/redmine-${MAJOR}-${MINOR}-${PATCH}/configdatabase.yml" /opt/oliveheart/redmine-${MAJOR}-${MINOR}-${PATCH}/config/database.yml.example &&
     mv /opt/oliveheart/redmine-${MAJOR}-${MINOR}-${PATCH} /var/www/redmine &&
     cd /var/www/redmine &&
