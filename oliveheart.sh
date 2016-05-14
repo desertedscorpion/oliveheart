5f328ac0152d26c0bdc44783d04d417e2588dc47
Emory Merryman <emory.merryman@gmail.com>
2016-05-14 22:27:09 +0000

    some fixes

--- oliveheart.sh
+++ oliveheart.sh
@@ -4,10 +4,9 @@ PASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1) &&
     PASSWORD=password && // remove me
     echo -en "\n\n${PASSWORD}\n${PASSWORD}\n\n\n\n" | mysql_secure_installation &&
     sed -e "s#password#${PASSWORD}#" /opt/oliveheart/init.sql | mysql -u root --password=${PASSWORD} &&
-    sed -e "s#username: root#username: redmine#" -e "s#password: \"\"#password: \"${PASSWORD}\"#" -e "w/opt/oliveheart/redmine-${MAJOR}-${MINOR}-${PATCH}/configdatabase.yml" /opt/oliveheart/redmine-${MAJOR}-${MINOR}-${PATCH}/config/database.yml.example &&
-    mv /opt/oliveheart/redmine-${MAJOR}-${MINOR}-${PATCH} /var/www/redmine &&
+    sed -e "s#username: root#username: redmine#" -e "s#password: \"\"#password: \"${PASSWORD}\"#" -e "w/opt/oliveheart/redmine-${MAJOR}.${MINOR}.${PATCH}/config/database.yml" /opt/oliveheart/redmine-${MAJOR}.${MINOR}.${PATCH}/config/database.yml.example &&
+    mv /opt/oliveheart/redmine-${MAJOR}.${MINOR}.${PATCH} /var/www/redmine &&
     cd /var/www/redmine &&
-    mkdir /var/www/redmine/public/plugin_assets &&
     chown apache:apache --recursive /var/www/redmine/{files,log,public/plugin_assets,tmp} &&
     /usr/local/bin/bundle install --without development test &&
     mkdir --parents /usr/local/lib64/ruby/site_ruby/mysql2 &&
